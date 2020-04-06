00990a164cfd7e93dd4dcfdee37fa15440920cb7
Michael Sartin-Tarm <msartintarm@gmail.com>
2020-04-06 00:13:07 -0700

    added scroll stuff

--- server/client/src/components/Wrapper.js
+++ server/client/src/components/Wrapper.js
@@ -1,12 +1,15 @@
 import React, {Component} from "react";
-import './Wrapper.css';
 import { Link } from "react-router-dom";
+
+import './Wrapper.css';
+import Scroll1 from './scrollz/scroll1.png';
 
 class Wrapper extends Component {
     render() {
 	return (
 		<div className="Wrapper">
 		Wrapper
+		<img src={Scroll1} />
 		{this.props.children}
 		</div>
 	);
