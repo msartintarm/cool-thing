cb3be119fab66595404845444ab8cd62825b4ca1
Michael Sartin-Tarm <msartintarm@gmail.com>
2020-04-06 03:40:08 +0000

    split everything out into components

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Intro from "./components/Intro";
+import Game from "./components/Game";
+import About from "./components/About";
 
 function App() {
     return (
+	<Router>
 	    <div className="App">
 	    <h1>Hey there</h1>
+	    <p>Contents:</p>
+            <Route exact path="/" component={Intro} />
+	    <Route exact path="/game" component={Game} />
+	    <Route exact path="/about" component={About} />
 	    </div>
+	    </Router>
     );
 }
 
