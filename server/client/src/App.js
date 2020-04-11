faa938cc7b924714aebbe04c6655c75551f5a2de
Michael Sartin-Tarm <msartintarm@gmail.com>
2020-04-11 08:36:25 +0000

    patched border images up using flexbox

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Wrapper from "./components/Wrapper";
 
@@ -11,14 +11,29 @@ import About from "./components/About";
 function App() {
     return (
 	    <Router>
-	    <Wrapper>
+	    <Wrapper title={
+		    <Switch>
+		    <Route exact path="/">
+		    Hello
+		    </Route>
+		    <Route exact path="/game">
+		    Game
+		    </Route>
+		    <Route exact path="/about">
+		    About
+		    </Route>
+		    </Switch>
+	    }
+	content1={
 	    <div className="App">
 	    <h1>Hey there</h1>
-	    <p>Contents:</p>
+	    <Switch>
             <Route exact path="/" component={Intro} />
 	    <Route exact path="/game" component={Game} />
 	    <Route exact path="/about" component={About} />
+	    </Switch>
 	    </div>
+	}>
 	    </Wrapper>
 	    </Router>
     );
--- server/client/src/components/Wrapper.js
+++ server/client/src/components/Wrapper.js
@@ -3,17 +3,53 @@ import { Link } from "react-router-dom";
 
 import './Wrapper.css';
 import Scroll1 from './scrollz/scroll1.png';
+import Scroll2 from './scrollz/scroll2.png';
+import Scroll3 from './scrollz/scroll3.png';
+import Scroll4 from './scrollz/scroll4.png';
+import Scroll5 from './scrollz/scroll5.png';
+import Scroll6 from './scrollz/scroll6.png';
+import Scroll7 from './scrollz/scroll7.png';
+import Scroll8 from './scrollz/scroll8.png';
+import Scroll9 from './scrollz/scroll9.png';
+// import Scroll10 from './scrollz/scroll10.png';
+// import Scroll11 from './scrollz/scroll11.png';
+// import Scroll12 from './scrollz/scroll12.png';
 
 class Wrapper extends Component {
     render() {
 	return (
 		<div className="Wrapper">
-		Wrapper
-		<img src={Scroll1} />
-		{this.props.children}
+
+		<div className="WrapperBackground">
+		{WrapperSection(Scroll1, Scroll2, Scroll3)}
+	    {WrapperSection(Scroll7, Scroll8, Scroll9, " WrapperScroll")}
+	    {WrapperSection(Scroll4, Scroll5, Scroll6)}
+		</div>
+
+		<div className="WrapperContent">
+		<div className="WrapperTitle">{this.props.title}</div>
+		<div className="WrapperParag">{this.props.content1}</div>
+		</div>
 		</div>
 	);
     }
+}
+//	    {WrapperSection(Scroll7, Scroll8, Scroll9)}
+//	    {WrapperSection(Scroll10, Scroll11, Scroll12)}
+
+function WrapperSection(sectionL, sectionCenter, sectionR, extraClasses="") {
+    return <div className={`WrapperSection${extraClasses}`}>
+	{WrapperBannerImage(sectionL)}
+    {WrapperBannerImage(sectionCenter, " WrapperScroll")}
+    {WrapperBannerImage(sectionR)}
+    </div>;
+}
+
+function WrapperBannerImage(content, extraClasses="") {
+    return <img className={`WrapperBanner${extraClasses}`}
+    alt=""
+    src={content}
+    />;
 }
 
 export default Wrapper;
