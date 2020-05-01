635c682714536f2fa0bc31c1dd4e51f92d6474ed
Michael Sartin-Tarm <msartintarm@gmail.com>
2020-05-01 08:21:57 +0000

    cleaned up content

--- server/client/src/components/About.js
+++ server/client/src/components/About.js
@@ -5,9 +5,7 @@ class About extends Component {
 	return (
 	    <div>
 	      <p>
-		Michael Sartin-Tarm enjoys cheese, running, and beer. </p>
-	      <p>
-		He created this site to spread platforming joy. First, though, he has to learn how to create games. To be continued...
+		Michael Sartin-Tarm created this site to spread platforming joy. First, though, he has to learn how to create games. Hopefully he'll finish something soon.
 	      </p>
 	    </div>
 	);
--- server/client/src/components/Game.js
+++ server/client/src/components/Game.js
@@ -13,10 +13,12 @@ class Game extends Component {
 	return (
 	    <div className={this.props.isOpen? "GameContainerOpen": ""}>
 	    <div className="GameBannerContainer">
-	    <span className="GameDescription">Welcome to the game!</span>
+	      <p>Welcome to the game!</p>
 	    {renderButton(this.props.isOpen, this.props.expandFn)}
 	    </div>
 	    {this.props.isOpen && renderGame()}
+	      <p>This is a short demo written in Unity and compiled to WebAssembly.</p>
+	      <p>Control "5" using the arrow keys, get the dollar bill, and use your mighty scissors to push the creeps off the edge! To be continued...</p>
 	    </div>
 	);
     }
