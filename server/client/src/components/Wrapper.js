adecacd7a318928c62b50662da1d1cbdb94d4474
Michael Sartin-Tarm <msartintarm@gmail.com>
2020-05-03 23:34:05 +0000

    wrapper logic now sets image size on DOM once images have loaded. replaced existing favicon with favicons for all platforms

--- server/client/src/components/Wrapper.js
+++ server/client/src/components/Wrapper.js
@@ -16,15 +16,19 @@ import Scroll9 from './scrollz/scroll9.png';
 
 const IMAGE_CLASS = "WrapperBanner";
 
+// This is a wrapper class (basically chrome) which wraps the page
+// It positions 9 images as borders. basically 9-slice scaling: https://en.wikipedia.org/wiki/9-slice_scaling
+// It smoothly shrinks the images when the provided scale shrinks from the initial value
 class Wrapper extends Component {
     constructor(props) {
 	super(props);
 	this.state = {
+	    initialScale: props.scale,
 	    initialImageDimensions: undefined,
-	    imgDim: [[],[],[],[],[],[],[],[],[],[],[],[]],
-	    scale: 1
+	    imgDim: new Array(9).fill([]),
 	};
-	this.imageEls_ = [];
+	this.imageLoadsLeft_ = 9;
+	this.checkIfImagesHaveLoaded_ = this.checkIfImagesHaveLoaded_.bind(this);
     }
     
     render() {
@@ -33,9 +37,9 @@ class Wrapper extends Component {
 
 	      <div className="WrapperBackground">
 		{/* order of the imgDim array doesn't matter */}
-		{WrapperSection([Scroll1, Scroll2, Scroll3], this.state.imgDim.slice(0,3))}
-		{WrapperSection([Scroll7, Scroll8, Scroll9], this.state.imgDim.slice(3,6), /** repeat= */ true)}
-		{WrapperSection([Scroll4, Scroll5, Scroll6], this.state.imgDim.slice(6,9))}
+		{this.renderWrapperSection([Scroll1, Scroll2, Scroll3], this.state.imgDim.slice(0,3))}
+		{this.renderWrapperSection([Scroll7, Scroll8, Scroll9], this.state.imgDim.slice(3,6), /** repeat= */ true)}
+		{this.renderWrapperSection([Scroll4, Scroll5, Scroll6], this.state.imgDim.slice(6,9))}
 	      </div>
 
 	      <div className="WrapperContent">
@@ -46,29 +50,62 @@ class Wrapper extends Component {
 	);
     }
 
-	componentDidMount() {
-	    this.imageEls_ = [...document.getElementsByClassName(IMAGE_CLASS)];
-	}
+    renderWrapperSection([sectionL, sectionCenter, sectionR], [sizeL, sizeC, sizeR], repeat=false) {
+	// don't apply height to portion which repeats vertically
+	const [appliedSizeL, appliedSizeC, appliedSizeR] = [sizeL, sizeC, sizeR].map(
+	    ([x, y]) => [x, (repeat? "": y)]
+	);
+    
+	return <div className={`WrapperSection${repeat? " WrapperRepeat": ""}`}>
+	  {this.renderWrapperBannerImage(sectionL, appliedSizeL)}
+	  {this.renderWrapperBannerImage(sectionCenter,  appliedSizeC, /** repeat= */ true)}
+	  {this.renderWrapperBannerImage(sectionR, appliedSizeR)}
+	</div>;
+    }
+    
+    renderWrapperBannerImage(content, [sizeX, sizeY], repeat=false) {
+	// don't apply width to portion which repeats horizontally
+	const appliedX = repeat? "": sizeX;
+	return <img className={`${IMAGE_CLASS}${repeat? " WrapperRepeat": ""}`}
+	alt=""
+	src={content}
+	width={appliedX}
+	height={sizeY}
+	onLoad={this.checkIfImagesHaveLoaded_}
+	/>;
+    }
 
     componentDidUpdate(prevProps) {
 	if (this.props.scale === prevProps.scale) {
 	    return;
 	}
 
-	return (this.props.scale !== 1)?
-	       this.shrinkSelf(this.props.scale):
+	return (this.props.scale !== this.state.initialScale)?
+	       this.shrinkSelf(this.props.scale / this.state.initialScale):
 	       this.unshrinkSelf();
+    }
+
+    checkIfImagesHaveLoaded_() {
+	this.imageLoadsLeft_ -= 1;
+	if (this.imageLoadsLeft_ === 0) {
+	    this.setInitialImageSizes();
+	}
+    }
+    
+    // collect initial dimensions of images and set them on the DOM so they can
+    // be animated when they change
+    setInitialImageSizes(newScale) {
+	const initialDimensions = getImageDimensions([...document.getElementsByClassName(IMAGE_CLASS)]);
+	this.setState({
+	    initialImageDimensions: initialDimensions,
+	    imgDim: initialDimensions,
+	});
     }
     
     // collects initial dimensions of images. sets new dimensions
     shrinkSelf(newScale) {
-	const initialDimensions = this.state.initialImageDimensions
-			       || getImageDimensions(this.imageEls_);
-	const newDimensions = scaleDimensions(initialDimensions, newScale);
-	
 	this.setState({
-	    initialImageDimensions: initialDimensions,
-	    imgDim: newDimensions,
+	    imgDim: scaleDimensions(this.state.initialImageDimensions, newScale),
 	});
     }
 
@@ -91,27 +128,5 @@ function scaleDimensions(dimensions, scale) {
     return dimensions.map(([x,y]) => [x * scale, y * scale]);
 }
 
-function WrapperSection([sectionL, sectionCenter, sectionR], [sizeL, sizeC, sizeR], repeat=false) {
-    // don't apply height to the portion of the banner which repeats vertically
-    const [appliedSizeL, appliedSizeC, appliedSizeR] = [sizeL, sizeC, sizeR].map(
-	([x, y]) => [x, (repeat? "": y)]
-    );
-    
-    return <div className={`WrapperSection${repeat? " WrapperRepeat": ""}`}>
-      {WrapperBannerImage(sectionL, appliedSizeL)}
-      {WrapperBannerImage(sectionCenter,  appliedSizeC, /** repeat= */ true)}
-      {WrapperBannerImage(sectionR, appliedSizeR)}
-    </div>;
-}
-
-function WrapperBannerImage(content, [sizeX, sizeY], repeat=false) {
-    const appliedX = repeat? "": sizeX;
-    return <img className={`${IMAGE_CLASS}${repeat? " WrapperRepeat": ""}`}
-		alt=""
-		src={content}
-		width={appliedX}
-		height={sizeY}
-    />;
-}
 
 export default Wrapper;
