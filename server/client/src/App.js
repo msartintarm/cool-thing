8ded6e8b89022bc01a9034a00e696abb53784fad
Michael Sartin-Tarm <msartintarm@gmail.com>
2020-05-03 01:52:46 +0000

    rearrange in prep to add more links

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch, useRouteMatch } from "react-router-dom";
 
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -14,45 +14,43 @@ class App extends React.PureComponent {
     constructor(props) {
 	super(props);
 	this.state = {
-	    unityGameExpanded: false,
+	    gameExpanded: false,
 	    wrapperScale: 1.0
 	};
-	this.expandUnityGame = this.expandUnityGame.bind(this);
+	this.expandGame = this.expandGame.bind(this);
     }
     
     render() {
-
 	const titleContent = 
 	    <h1>
 	      <Switch>
-		<Route exact path="/">Hello</Route>
-		<Route exact path="/game">Game</Route>
-		<Route exact path="/about">About</Route>
+		{[
+		     ["/", "Hello"],
+		     ["/game", "Game"],
+		     ["/about","About"],
+		].map(([link, text]) => (<Route exact path={link}>{text}</Route>))}
 	      </Switch>
 	    </h1>;
-
-	const footerLinks = new Map([
-	    ["Check out the intro", "/"],
-	    ["Check out the game", "/game"],
-	    ["About the site", "/about"],
-	]);
-
+	
 	return (
 	    <div className="App">
 	      <Router>
 		<Wrapper
-		    title={!this.state.unityGameExpanded && titleContent}
+		    title={!this.state.gameExpanded && titleContent}
 		    scale={this.state.wrapperScale}
 		    content1={
 			<>
 			  <Switch>
 			    <Route exact path="/"><Intro /></Route>
 			    <Route exact path="/game">
-			      <UnityGame isOpen={this.state.unityGameExpanded} expandFn={this.expandUnityGame} />
+			      <UnityGame isOpen={this.state.gameExpanded} expandFn={this.expandGame} />
+			    </Route>
+			    <Route exact path="/game2">
+			      <PhaserGame isOpen={this.state.gameExpanded} expandFn={this.expandGame} />
 			    </Route>
 			    <Route exact path="/about"><About /></Route>
 			  </Switch>
-			  {!this.state.unityGameExpanded && <Footer linkz={footerLinks}/>}
+			  {!this.state.gameExpanded && <FooterWithLinks/>}
 			</>
 		    }/>
 	      </Router>
@@ -60,13 +58,30 @@ class App extends React.PureComponent {
 	);
     }
     
-    expandUnityGame() {
-	const newExpandedState = !this.state.unityGameExpanded;
+    expandGame() {
+	const newExpandedState = !this.state.gameExpanded;
 	this.setState({
-	    unityGameExpanded: newExpandedState,
+	    gameExpanded: newExpandedState,
 	    wrapperScale: newExpandedState? 0.5: 1.0,
 	});
     }
+}
+
+/// Define the link mapping at the application level, not within the footer level
+function FooterWithLinks() {
+    // useRouteMatch is a 'react hook', and thus must obey certain constraints such as no dynamic inputs
+    const [_, matchingLink] = [
+	["/", useRouteMatch("/").isExact],
+	["/about", useRouteMatch("/about")],
+	["/game", useRouteMatch("/game")],
+    ].find(([_, isMatch]) => isMatch);
+    
+    const footerLinks = new Map([
+	["/", "Check out the intro"],
+	["/game", "Check out the game"],
+	["/about", "About the site"],
+    ]);
+    return (<Footer linkz={footerLinks} currLink = {matchingLink}/>);
 }
 
 export default App;
--- server/client/src/components/Footer.js
+++ server/client/src/components/Footer.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { Link, useRouteMatch } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './Footer.css';
 
 const DISABLED_CLASS = "FooterLinkDisabled";
 
 function Footer(props) {
-
-    const routeMatchMap = new Map([
-	["/", useRouteMatch("/").isExact],
-	["/about", useRouteMatch("/about")],
-	["/game", useRouteMatch("/game")],
-    ]);
-    
     return (
     <div className="FooterContainer">
       <div className="FooterSpacer"></div>
       <div className="FooterContent">
 	<p>
-	  {[...props.linkz.entries()].map(([content, uri]) =>
-	      <span key={uri} className={routeMatchMap[uri]? DISABLED_CLASS: ""}>
-		<Link to={uri}>{content}</Link>
+	  {[...props.linkz.entries()].map(([link, content]) =>
+	      <span key={link} className={props.currLink === link? DISABLED_CLASS: ""}>
+		<Link to={link}>{content}</Link>
 		<br/>
 	      </span>
 	  )}
